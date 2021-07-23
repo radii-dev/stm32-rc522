fcaf624dfd9a4ba0b79a903f88bf4d34b80d7c0b
Seongwook Kang <gigaslender2@gachon.ac.kr>
2021-07-23 17:13:37 +0900

    Add a project that has already been completed

---
+++ RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,25 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'RC522' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+/*  Keil::Device:STM32Cube Framework:STM32CubeMX:1.0.0 */
+#define RTE_DEVICE_FRAMEWORK_CUBE_MX
+/*  Keil::Device:Startup:2.6.3 */
+#define RTE_DEVICE_STARTUP_STM32F4XX    /* Device Startup for STM32F4 */
+
+
+#endif /* RTE_COMPONENTS_H */
